78381eb8012b8d4c496527a2e442fab2301f7732
Justin Wilkinson <wilkinsense@gmail.com>
2016-11-18 12:47:46 -0500

    Added triggers, prefabs, and collision fun.

---
+++ Assets/Scripts/CheckTriggerCollision.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CheckTriggerCollision : MonoBehaviour {
+
+  /* If you're working with a triggered collision (intersection),
+   * this is called when the triggered collision starts. */
+  void OnTriggerEnter(Collider collider) {
+    Debug.Log(collider.gameObject.name + " entered the trigger!");
+  }
+
+  /* If you're working with a triggered collision (intersection),
+   * this is called as the triggered collision continues to occur. */
+  void OnTriggerStay(Collider collider) {
+    Debug.Log(collider.gameObject.name + " stayed in the trigger!");
+  }
+
+  /* If you're working with a triggered collision (intersection),
+   * this is called when the triggered collision ends. */
+  void OnTriggerExit(Collider collider) {
+    Debug.Log(collider.gameObject.name + " exited the trigger!");
+  }
+
+  // If you're doing standard collision.
+  void OnCollisionEnter(Collision collision) {
+
+  }
+}
